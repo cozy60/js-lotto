7c4150a4f25beaf8ed8520e1cc7f9b9c23ea9795
yukyung <s2018w39@e-mirim.hs.kr>
2022-03-22 16:45:06 +0900

    feat: step1 요구사항 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,63 @@
-const $showResultButton = document.querySelector('.open-result-modal-button')
-const $modalClose = document.querySelector('.modal-close')
-const $modal = document.querySelector('.modal')
-const $lottoNumbersToggleButton = document.querySelector(
-  '.lotto-numbers-toggle-button'
-)
+import {
+	$lottoForm,
+	$lottoFormInput,
+	$lottoResult,
+	$lottoNumber,
+	$toggle,
+	$lottoPurchases,
+	$lottoNumberResult,
+} from './util/dom.js';
 
-const onModalShow = () => {
-  $modal.classList.add('open')
-}
+const lottoMaker = () => {
+	const lotto = [];
 
-const onModalClose = () => {
-  $modal.classList.remove('open')
-}
+	for (let i = 0; i < 6; i += 1) {
+		const num = Math.floor(Math.random() * 45) + 1;
 
-$showResultButton.addEventListener('click', onModalShow)
-$modalClose.addEventListener('click', onModalClose)
+		if (lotto.indexOf(num) < 0) {
+			lotto.push(num);
+		}
+	}
+
+	return lotto;
+};
+
+$lottoForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const price = $lottoFormInput.value;
+	if (price % 1000 !== 0) {
+		alert('값은 1,000단위로 입력 가능합니다.');
+		return;
+	}
+
+	const numberPurchases = price / 1000;
+	const lottoList = [];
+
+	$lottoResult.style.display = 'block';
+	$lottoNumber.style.display = 'block';
+
+	for (let i = 0; i < numberPurchases; i += 1) {
+		lottoList.push({ icon: '🎟️', number: lottoMaker() });
+	}
+
+	$lottoPurchases.innerText = `총 ${numberPurchases}개를 구매하였습니다.`;
+
+	const template = lottoList
+		.map(
+			(
+				lotto
+			) => `<li class="mx-1 text-4xl lotto-wrapper"><span class="lotto-icon">${lotto.icon} </span>
+			<span class="lotto-detail" style="display: none;">${lotto.number}</span></li>`
+		)
+		.join('');
+	$lottoNumberResult.innerHTML = template;
+});
+
+let hide = true;
+$toggle.addEventListener('click', (e) => {
+	hide = !hide;
+	const lottoNumberResult = document.querySelectorAll('.lotto-detail');
+	Array.from(lottoNumberResult).map(
+		(item) => (item.style.display = hide ? 'none' : 'block')
+	);
+});
---
+++ src/js/util/dom.js
@@ -0,0 +1,9 @@
+import { $ } from './helper.js';
+
+export const $lottoForm = $('#lottoForm');
+export const $lottoFormInput = $('#lottoFormInput');
+export const $lottoResult = $('#lottoResult');
+export const $lottoNumber = $('#lottoNumber');
+export const $toggle = $('#toggle');
+export const $lottoPurchases = $('#lottoPurchases');
+export const $lottoNumberResult = $('#lottoNumberResult');
